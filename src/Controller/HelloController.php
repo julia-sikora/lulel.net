9dbad6c72480b6680e12e7ddb80e72e6a5baf967
tujuleczka <julia.sikora.poczta@gmail.com>
2022-12-02 19:34:44 +0100

    add file HelloController.php

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response("Hello!");
+    }
+}
