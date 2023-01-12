f0ee27f1bedab1f8738674bfde002fc7daa1493c
tujuleczka <julia.sikora.poczta@gmail.com>
2023-01-12 17:06:26 +0100

    Modify languages changing

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,16 +7,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Translation\LocaleSwitcher;
 
 class HelloController extends AbstractController
 {
-    private const DEFAULT_LANGUAGE = 'en';
-
-    #[Route('/{lang}', name: 'app_homepage_lang')]
-    public function homepage(LocaleSwitcher $localeSwitcher, string $lang = self::DEFAULT_LANGUAGE): Response
+    #[Route('/', name: 'app_homepage')]
+    public function homepage(): Response
     {
-        $localeSwitcher->setLocale($lang );
         return $this->render('homepage.html.twig');
     }
 }
